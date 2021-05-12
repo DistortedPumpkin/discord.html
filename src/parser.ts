6b90dbebe9780835c56617947f8404cad53db243
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-05-11 20:16:29 -0400

    Update parser.ts
    not finished

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,5 @@
 import { HTMLBot } from "./bot";
+import * as cheerio from 'cheerio';
 
 export class Parser {
 
@@ -9,3 +10,19 @@ export class Parser {
     }
 
 }
+
+let x = cheerio.load(`
+<head>
+    <data name="token">TOKEN HERE</data>
+    <data name="prefix">!</data>
+    <data name="status">Running on HTML!</data>
+</head>`)
+x('data').each((i, data) => {
+  
+  const stuff = `${data.attribs.name}: ${x(data).text()} `
+
+  console.log(stuff)
+})
+
+
+
