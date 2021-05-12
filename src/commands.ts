c2807dacbb564b172e920297720954d3d7b7006c
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-05-12 18:22:16 -0400

    Create commands.ts
    command class (credit to dragon for this)

---
+++ src/commands.ts
@@ -0,0 +1,28 @@
+class Command{
+  readonly name: string;
+  actions: Action[];
+  constructor(name: string){
+    this.name = name;
+    actions = [];
+  }
+}
+
+abstract class Action {
+    readonly actionType: string;
+    abstract execute(): void;
+    constructor(actionType: string){
+      this.actionType = actionType;
+    }
+}
+
+class SendMessageAction extends Action {
+
+  constructor(data: any){
+    super("sendMessage");
+  }
+  
+  async execute(bot: HTMLBot, context: any, data: any): Promise<void> {
+      const channelID = data["channel"] || context["channel"];
+      return bot.createMessage(channelID, {content: data["content"]});
+  }  
+}
