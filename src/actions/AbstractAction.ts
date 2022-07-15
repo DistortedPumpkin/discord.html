15c31a93d91b8540b8cb1e04e27e95076e263a0f
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2022-07-15 10:06:55 -0400

    Delete src directory
    Recreating everything in python

--- src/actions/AbstractAction.ts
+++
@@ -1,9 +0,0 @@
-import { HTMLBot } from "../bot";
-
-export abstract class AbstractAction {
-    readonly actionType: string;
-    abstract execute(bot: HTMLBot, context: any): any | Promise<any>;
-    constructor(actionType: string) {
-        this.actionType = actionType;
-    }
-}
