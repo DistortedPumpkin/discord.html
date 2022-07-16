2fbb131a83c8d2284e12faf809c5ab09fedf2ab4
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2022-07-16 01:44:24 -0400

    more package stuff + bumped version to 0.0.5

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from pathlib import Path
+from setuptools import setup, find_packages
 import re
 
+
+ROOT = Path(__file__).parent
 
 version = ''
 with open('discordhtml/__init__.py') as f:
@@ -12,19 +15,22 @@ with open('README.md') as f:
 if not version:
     raise RuntimeError('version is not set')
 
+with open(ROOT / 'requirements.txt') as f:
+    requirements = f.read()
+
 setup(
     name='discord.html',
     version=version,
     description='A package to interact with the Discord API by writing HTML',
     long_description=readme,
     author='DistortedPumpkin',
-    install_requires=['discord.py==2.0.0a', 'bs4'],
-    dependency_links=['git+https://github.com/Rapptz/discord.py.git@c7e589033df300d0b305ca43ff7f1de361c9c4af', 'bs4'],
+    dependency_links=['http://github.com/Rapptz/discord.py/tarball/master#egg=discord', 'bs4'],
     python_requires='>=3.8.0',
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha"
-    ]
+    ],
+    packages=find_packages()
 )
